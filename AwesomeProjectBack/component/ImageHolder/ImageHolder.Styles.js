9cbe23fad1c2469ba7e48c2ced20e68200348afa
Madhu <mankalmadhu@gmail.com>
2017-08-04 03:44:41 +0530

    Remove unneeded directories

--- AwesomeProjectBack/component/ImageHolder/ImageHolder.Styles.js
+++
@@ -1,5 +0,0 @@
-import {StyleSheet} from 'react-native';
-
-module.exports  = StyleSheet.create({
-  dimensions : {width: 193, height: 110}
-});
--- AwesomeProjectBack/component/sum/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./sum');
-
-test("add two digits", () => {
-    expect(sum(1,2)).toBe(3);
-});
